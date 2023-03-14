862420f3e035819415757c4a864f93988283bd73
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-03-14 12:50:45 +0100

    Add boilerplate to generate a CodeMirror language

---
+++ packages/codemirror-language/src/index.ts
@@ -0,0 +1,38 @@
+import {
+  delimitedIndent,
+  foldInside,
+  foldNodeProp,
+  indentNodeProp,
+  LanguageSupport,
+  LRLanguage,
+} from "@codemirror/language";
+import { styleTags, tags as t } from "@lezer/highlight";
+
+import { parser } from "./syntax.grammar";
+
+export const LiveblocksSchemaLanguage = LRLanguage.define({
+  parser: parser.configure({
+    props: [
+      indentNodeProp.add({
+        Application: delimitedIndent({ closing: ")", align: false }),
+      }),
+      foldNodeProp.add({
+        Application: foldInside,
+      }),
+      styleTags({
+        Identifier: t.variableName,
+        Boolean: t.bool,
+        String: t.string,
+        LineComment: t.lineComment,
+        "( )": t.paren,
+      }),
+    ],
+  }),
+  languageData: {
+    commentTokens: { line: ";" },
+  },
+});
+
+export function liveblocksSchemaLanguage(): LanguageSupport {
+  return new LanguageSupport(LiveblocksSchemaLanguage);
+}
