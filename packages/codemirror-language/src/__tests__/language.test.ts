8d8d1d24dce779a92b09d80ef97bc97b0ec7bd38
Marc Bouchenoire <bouchenoire.marc@gmail.com>
2023-03-16 17:59:38 +0100

    Improve tests

--- packages/codemirror-language/src/__tests__/language.test.ts
+++ packages/codemirror-language/src/__tests__/language.test.ts
@@ -1,21 +1,70 @@
-/* eslint-disable @typescript-eslint/no-var-requires */
-// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-// @ts-ignore
-const { LiveblocksSchema } = require("../../dist/index.js");
-const fs = require("fs/promises");
-const path = require("path");
+import fs from "fs";
+import path from "path";
+
+import { LiveblocksSchema } from "../../dist/index.js";
 
 const ERROR_CHARACTER = "⚠";
 
-describe("LiveblocksSchema", () => {
-  it("should TODO", async () => {
-    const parser = LiveblocksSchema.parser;
-    console.log(process.cwd());
-    const schema = await fs.readFile(
-      path.resolve(process.cwd(), "./src/__tests__/example.schema"),
-      "utf-8"
-    );
+// TODO: Share helpers with /packages/liveblocks-schema/src/__tests__/examples.test.ts
 
-    expect(parser.parse(schema).toString()).not.toContain(ERROR_CHARACTER);
+function* readFiles(dirpath: string): IterableIterator<string> {
+  const files = fs.readdirSync(dirpath);
+  for (const file of files) {
+    if (file.startsWith(".")) {
+      continue;
+    }
+
+    const filePath = path.join(dirpath, file);
+    const fileStat = fs.statSync(filePath);
+    if (fileStat.isDirectory()) {
+      yield* readFiles(filePath);
+    } else if (fileStat.isFile()) {
+      yield path.join(filePath);
+    }
+  }
+}
+
+function readExamplesSync(filename: string): string[] {
+  return fs.readFileSync(filename, { encoding: "utf-8" }).split("---\n");
+}
+
+function declareJestTest(filename: string) {
+  const basename = path.basename(filename);
+  return /\bskip\b/i.test(basename)
+    ? it.skip
+    : /\bfail\b/i.test(basename)
+    ? it.failing
+    : it;
+}
+
+type TestSrc = readonly [string, string, string];
+//                         /        |        \
+//                    File path  Test name  Contents
+
+describe("LiveblocksSchema", () => {
+  const parser = LiveblocksSchema.parser;
+  const exampleFiles: string[] = Array.from(
+    readFiles(path.join(__dirname, "../../../liveblocks-schema/examples/good"))
+  );
+
+  // Each file can consist of multiple "sections", each being a test of its
+  // own.
+  const exampleTests: readonly TestSrc[] = exampleFiles.flatMap((filename) => {
+    const chunks = readExamplesSync(filename);
+    return chunks.map((content, index) => {
+      return [
+        filename,
+        chunks.length > 1
+          ? `${path.basename(filename)} [${index + 1}/${chunks.length}]`
+          : path.basename(filename),
+        content,
+      ] as const;
+    });
+  });
+
+  exampleTests.map(([f, name, content]) => {
+    declareJestTest(f)(name, () => {
+      expect(parser.parse(content).toString()).not.toContain(ERROR_CHARACTER);
+    });
   });
 });
